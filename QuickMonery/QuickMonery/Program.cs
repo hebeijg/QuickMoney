ad5044ba5a82e335993bcf56fe358438da71343b
zhangruidong <zhangmoumout@163.com>
2019-09-25 22:49:30 +0800

    提交图片

--- QuickMonery/QuickMonery/Program.cs
+++ QuickMonery/QuickMonery/Program.cs
@@ -15,7 +15,7 @@ namespace QuickMonery
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginFrm());
         }
     }
 }
