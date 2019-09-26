dd473baaedd28616c4e07e751167054d0909ea58
shenguanjun <53102830+shenguanjun@users.noreply.github.com>
2019-09-26 23:40:58 +0800

    范总提的

---
+++ QuickMonery/QuickMonery.Common/IniFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+
+namespace QuickMonery.Common
+{
+    public class IniFile
+    {
+        public string path;     //INI文件名
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        /// <summary>
+        /// 添加section
+        /// </summary>
+        /// <param name="section">section name</param>
+        /// <param name="keys">键值对,多个用\0分隔，如key1=value1\0key2=value2</param>
+        /// <param name="filePath">ini文件路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32",CharSet=CharSet.Auto )]
+        private static extern bool WritePrivateProfileSection(string lpAppName, string lpString, string lpFileName);
+
+        //声明读写INI文件的API函数     
+        public IniFile(string INIPath)
+        {
+            path = INIPath;
+        }
+
+        //类的构造函数，传递INI文件名
+        public  void IniWriteValue(string Section, string Key, string Value)
+        {
+            WritePrivateProfileString(Section, Key, Value, this.path);
+        }
+
+        //读INI文件         
+        public string IniReadValue(string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(256);
+            int i = GetPrivateProfileString(Section, Key, "", temp, 256, this.path);
+            return temp.ToString();
+        }
+        /// <summary>
+        /// 添加ini配置项到某个节点，如果节点不存在则创建
+        /// </summary>
+        /// <param name="sectionName">节点名称</param>
+        /// <param name="keys">多个用\0分隔，如key1=value1\0key2=value2</param>
+        /// <param name="filePath">ini文件全路径名</param>
+        public void  IniCreateSection(string sectionName,string items ,string filePath)
+        {
+            try
+            {
+                bool b = WritePrivateProfileSection(sectionName, items, filePath);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
---
+++ QuickMonery/QuickMonery.Common/LogInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuickMonery.Common
+{
+    public class LogInfo
+    {
+        /// <summary>
+        /// 把异常信息写入到本地Log文件中
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="description"></param>
+        public static void WriteLog(string action, string description)
+        {
+            object lockObject = new object();
+            lock (lockObject)
+            {
+
+                try
+                {
+                    string logPath = "";
+                    if (string.IsNullOrEmpty(logPath))
+                    {
+                        logPath = Application.StartupPath + "\\Log";
+                    }
+                    if (!File.Exists(logPath))
+                    {
+                        Directory.CreateDirectory(logPath);
+                    }
+                    string filePath = logPath + "\\log_" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                    if (File.Exists(filePath))
+                    {
+                        FileInfo fi = new FileInfo(filePath);
+                        if (fi.Attributes.ToString().IndexOf("ReadOnly") != -1)
+                            fi.Attributes = FileAttributes.Normal;
+
+                    }
+                    FileStream fs = new FileStream(filePath, FileMode.Append);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " | " + action + " | " + description);
+                    sw.Close();
+                    fs.Close();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                
+            }
+        }
+
+    }
+}
